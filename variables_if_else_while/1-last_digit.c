219562daa6515b565aca8b986386d43fc05a78d1
ImamverdiyevEmil <9911@holbertonstudent.com>
2024-10-21 07:43:15 +0000

    new output

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,14 +2,14 @@
 #include <time.h>
 #include <stdio.h>
 
- /**
-  * main - Determines either greater than 5, is less than 6, or is 0
-  *
-  * Return: Always 0 (Success)
-  */
+/**
+ * main - Determines either greater than 5, is less than 6, or is 0
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int n, l;	
+	int n, l;
 
         srand(time(0));
 	n = rand() - RAND_MAX / 2;
