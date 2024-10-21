2f6cca30bf4d684916e46616e15d3b65399063d4
ImamverdiyevEmil <9911@holbertonstudent.com>
2024-10-21 08:38:02 +0000

    new output

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -9,18 +9,13 @@
  */
 int main(void)
 {
-	int i;	for (i = 97; i < 123; i++)
+	int i;
+
+	for (i = 97; i < 123; i++)
 	{
 		if (i != 101 && i != 113)
-		{
 			putchar(i);
-		}
 	}
 	putchar('\n');
 	return (0);
 }
-NEW
-
-
-
-
